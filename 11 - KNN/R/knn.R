90ff2f172154cadb9ee8debc24954bb879f7f78b
Thiago Montenegro <47747825+thimontenegro@users.noreply.github.com>
2019-07-20 03:45:45 -0300

    Add files via upload

---
+++ 11 - KNN/R/knn.R
@@ -0,0 +1,67 @@
+# K-Nearest Neighbors (K-NN)
+
+#Importing the dataset
+dataset = read.csv('Data/Social_Network_Ads.csv')
+dataset = dataset[3:5]
+
+#Enconding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0,1))
+
+#Splitting the dataset into Training set and Test Set
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+#Fitting KNN to the Training set and Predicting the Test  set result
+#Create your classifier
+library(class)
+y_pred = knn(train = training_set[, -3],
+             test = test_set[, -3],
+             cl = training_set[, 3],
+             k = 5)
+
+
+#Making confusion Matrix
+cm = table(test_set[, 3], y_pred)
+
+#Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1] + 1, by=0.01))
+X2 = seq(min(set[, 2]) - 1, max(set[, 2] + 1, by=0.01))
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = knn(train=grid_set,
+             test=test_set[, -3],
+             cl=training_set[, 3],
+             k=5)
+plot(set[, -3],
+     main='K-NN (Training Set)',
+     xlab='Age', ylab='Estimated Salary',
+     xlim=range(X1), ylim=range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add=TRUE)
+points(grid_set, pch='.', col=ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch=21, bg=ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+#Visualising the Test set result
+
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1] + 1, by=0.01))
+X2 = seq(min(set[, 2]) - 1, max(set[, 2] + 1, by=0.01))
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+prob_set = predict(classifier, type='response', newdata=grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main='K-NN (Test Set)',
+     xlab='Age', ylab='Estimated Salary',
+     xlim=range(X1), ylim=range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add=TRUE)
+points(grid_set, pch='.', col=ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch=21, bg=ifelse(set[, 3] == 1, 'green4', 'red3'))
